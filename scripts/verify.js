d8882a61f86b874f82d6c5c02967309064a8839e
Steven Enamakel <enamakel@vezures.com>
2021-02-04 21:32:09 +0300

    added deply scripts

---
+++ scripts/verify.js
@@ -0,0 +1,5 @@
+module.exports = [
+  '0x26f9Bd6af98a56D34bA49ce0B0359658Eecd77bE',
+  '0x0a180a76e4466bf68a7f86fb029bed3cccfaaac5'
+]
+// npx hardhat verify --constructor-args scripts/verify.js 0xc3B74c24Bc715A5608611C37Ea2a09659095ce76 --network ropsten
