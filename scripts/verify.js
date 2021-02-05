c25b9df80bc5f567b59466da530c5553ad76feae
Steven Enamakel <enamakel@vezures.com>
2021-02-05 12:12:45 +0300

    fixed unit test

--- scripts/verify.js
+++ scripts/verify.js
@@ -2,4 +2,4 @@ module.exports = [
   '0x26f9Bd6af98a56D34bA49ce0B0359658Eecd77bE',
   '0xc778417E063141139Fce010982780140Aa0cD5Ab'
 ]
-// npx hardhat verify --constructor-args scripts/verify.js 0xc3B74c24Bc715A5608611C37Ea2a09659095ce76 --network ropsten
+// npx hardhat verify --constructor-args scripts/verify.js 0xC5bF7563D3f70bF30c556D0C96668A8863479366 --network ropsten
--- test/UniswapV2Router01.spec.ts
+++ test/UniswapV2Router01.spec.ts
@@ -368,8 +368,8 @@ describe('UniswapV2Router{01,02}', () => {
           const receipt = await tx.wait()
           expect(receipt.gasUsed).to.eq(
             {
-              [RouterVersion.UniswapV2Router01]: 101876,
-              [RouterVersion.UniswapV2Router02]: 101898
+              [RouterVersion.UniswapV2Router01]: 106130,
+              [RouterVersion.UniswapV2Router02]: 106152
             }[routerVersion as RouterVersion]
           )
         }).retries(3)
@@ -517,8 +517,8 @@ describe('UniswapV2Router{01,02}', () => {
           const receipt = await tx.wait()
           expect(receipt.gasUsed).to.eq(
             {
-              [RouterVersion.UniswapV2Router01]: 138770,
-              [RouterVersion.UniswapV2Router02]: 138770
+              [RouterVersion.UniswapV2Router01]: 181424,
+              [RouterVersion.UniswapV2Router02]: 113024
             }[routerVersion as RouterVersion]
           )
         }).retries(3)
